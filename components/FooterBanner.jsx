89022a85d44963d58b4dda283bc15e03f5486050
farendivaz <farendivaz@gmail.com>
2022-05-05 20:53:38 +0700

    add products to the page & render it

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { urlfor } from '../lib/client';
 
-const FooterBanner = () => {
+const FooterBanner = ({footerBanner: {discount, largeText1, largeText2, saleTime, smallText, desc, midText, product, buttonText, image}}) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`product/${product}`}>
+            <button type='button'>{buttonText}</button>        
+          </Link>
+        </div>
+        <img src={urlfor(image)} className='footer-banner-image'/>
+      </div>
+    </div>
   )
 }
 
--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
+import { urlfor } from '../lib/client'
 
-const Product = () => {
+const Product = ({product: {image, name, slug, price}}) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`product/${slug.current}`}>
+        <div className='product-card'>
+          <img 
+            src={urlfor(image && image[0])} 
+            width={250} 
+            height={250}
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
